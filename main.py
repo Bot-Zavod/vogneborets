6ff80892096d130b092b6b3f3018b9504448c05f
Alexey <almarka03@gmail.com>
2019-12-14 00:20:14 +0200

    friendly not fire

--- main.py
+++ main.py
@@ -4,28 +4,40 @@ import logging
 from time import sleep
 from telegram import InlineKeyboardMarkup, InlineKeyboardButton
 
+
 def start_command(update, context):
 	sticker = "CAADAgAD_iAAAulVBRgi4A0qOPfBBRYE"
 	context.bot.sendSticker(chat_id=update.effective_chat.id, sticker=sticker)
-	text = "Привет, Михалыч епта!"
+	text = "Добрый день!"
 	context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
-	button1 = InlineKeyboardButton('кнопка1', callback_data='1')
-	button2 = InlineKeyboardButton('кнопка2', callback_data='2')
-	keyboard = InlineKeyboardMarkup([[button1, button2],[button1, button2]])
 
-	context.bot.sendMessage(chat_id=update.effective_chat.id, text='fuck', reply_markup=keyboard)
 
 def help_command(update, context):
 	sticker = "CAADAgADCSEAAulVBRjFPBc0MGnDUhYE"
 	context.bot.sendSticker(chat_id=update.effective_chat.id, sticker=sticker)
-	text = "Че надо блять?"
+	text = "*Чем могу помочь?*"
 	context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
 
+
 def unknown(update, context):
-	text = "Че ты пиздишь? Я не понимаю нахуй!"
+	text = "*Я вас не понимаю!(*"
 	context.bot.sendMessage(chat_id=update.effective_chat.id, text=text)
+	
+
+def test_alex(update, context):
+	# Inline Keyboard test#1
+	button1 = InlineKeyboardButton('кнопка1', callback_data='1')
+	button2 = InlineKeyboardButton('кнопка2', callback_data='2')
+	button3 = InlineKeyboardButton('кнопка3', callback_data='3')
+	button4 = InlineKeyboardButton('кнопка4', callback_data='4')
+	keyboard = InlineKeyboardMarkup([[button1, button2],[button3, button4]])
+	context.bot.sendMessage(chat_id=update.effective_chat.id, text='InlineKeyboard test#1', reply_markup=keyboard)
+
+	# Check message
 	text2 = "ЧТО ЭТО ТАКОЕ? *<"+update.message.text.upper()+'>*'
 	context.bot.sendMessage(chat_id=update.effective_chat.id, text=text2, parse_mode='markdown')
+
+
 
 
 if __name__ == "__main__":
@@ -39,6 +51,9 @@ if __name__ == "__main__":
 
 	help_handler = CommandHandler('help', help_command)
 	dispatcher.add_handler(help_handler)
+
+	test_alex = CommandHandler('test', test_alex)
+	dispatcher.add_handler(test_alex)
 
 	unknown_handler = MessageHandler(Filters.all, unknown)
 	dispatcher.add_handler(unknown_handler)
