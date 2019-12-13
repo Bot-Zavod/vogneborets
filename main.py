c2092605d7f6417f8366438b650d70d390717cc4
Vlad-Vargan <blach.smith.2000@gmail.com>
2019-12-14 00:11:33 +0200

    Location button

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from telegram.ext import Updater, CommandHandler
+from telegram import KeyboardButton, ReplyKeyboardMarkup
 from init import BotInitialize
 import logging
 
 
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+	location = KeyboardButton(text='Where are you', request_location = True)
+	reply_markup = ReplyKeyboardMarkup([[location]])
+	context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!", reply_markup=reply_markup)
 
 def helps(update, context):
 	context.bot.send_message(chat_id=update.effective_chat.id, text="How can i help you?")
@@ -24,6 +27,6 @@ if __name__ == "__main__":
 	help_handler = CommandHandler('help', helps)
 	dispatcher.add_handler(help_handler)
 
-	updater.start_polling()
-
 	
+
+	updater.start_polling()
