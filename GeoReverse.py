4b9f60901710c142d461b6977d6c2df45a0cc6c0
Vlad-Vargan <blach.smith.2000@gmail.com>
2019-12-17 21:41:57 +0200

    type difiner dict + function

--- GeoReverse.py
+++ GeoReverse.py
@@ -1,41 +1,62 @@
 from json import loads
-import urllib.request
+from urllib.parse import quote_plus
 from init import MapInitialize
 from requests import get
 # This module will help you while working
 # with adresses and coordinates using google maps api
 
-types = ['accounting', 'airport', 'amusement_park', 'aquarium', 'art_gallery', 'atm', 'bakery', 'bank', 'bar', 'beauty_salon', 'bicycle_store', 'book_store', 'bowling_alley', 'bus_station', 'cafe', 'campground', 'car_dealer', 'car_rental', 'car_repair', 'car_wash', 'casino', 'cemetery', 'church', 'city_hall', 'clothing_store', 'convenience_store', 'courthouse', 'dentist', 'department_store', 'doctor', 'drugstore', 'electrician', 'electronics_store', 'embassy', 'fire_station', 'florist', 'funeral_home', 'furniture_store', 'gas_station', 'grocery_or_supermarket', 'gym', 'hair_care', 'hardware_store', 'hindu_temple', 'home_goods_store', 'hospital', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'library', 'light_rail_station', 'liquor_store', 'local_government_office', 'locksmith', 'lodging', 'meal_delivery', 'meal_takeaway', 'mosque', 'movie_rental', 'movie_theater', 'moving_company', 'museum', 'night_club', 'painter', 'park', 'parking', 'pet_store', 'pharmacy', 'physiotherapist', 'plumber', 'police', 'post_office', 'primary_school', 'real_estate_agency', 'restaurant', 'roofing_contractor', 'rv_park', 'school', 'secondary_school', 'shoe_store', 'shopping_mall', 'spa', 'stadium', 'storage', 'store', 'subway_station', 'supermarket', 'synagogue', 'taxi_stand', 'tourist_attraction', 'train_station', 'transit_station', 'travel_agency', 'university', 'veterinary_care', 'zoo']
-
+types ={1:['primary_school', 'school', 'secondary_school', 'university'],
+        2:['bar', 'cafe', 'meal_delivery', 'meal_takeaway', 'restaurant'],
+        3:['accounting', 'atm', 'bakery', 'bank', 'beauty_salon', 'bicycle_store', 'book_store', 'bus_station', 'car_dealer', 'car_rental', 'car_repair', 'car_wash', 'clothing_store', 'convenience_store', 'dentist', 'department_store', 'doctor', 'drugstore', 'electrician', 'electronics_store', 'florist', 'gas_station', 'grocery_or_supermarket', 'hair_care', 'hardware_store', 'hindu_temple', 'home_goods_store', 'insurance_agency', 'jewelry_store', 'laundry', 'lawyer', 'liquor_store', 'locksmith', 'pet_store', 'pharmacy', 'physiotherapist', 'plumber', 'post_office', 'real_estate_agency', 'roofing_contractor', 'shoe_store', 'spa', 'storage', 'store', 'tourist_attraction', 'travel_agency', 'veterinary_care'],
+        4:['airport', 'amusement_park', 'aquarium', 'art_gallery','bowling_alley', 'casino', 'church', 'city_hall', 'embassy', 'fire_station', 'funeral_home', 'furniture_store', 'gym', 'hospital', 'library', 'light_rail_station', 'local_government_office', 'mosque', 'movie_rental', 'movie_theater', 'moving_company', 'museum', 'night_club', 'painter', 'park', 'parking', 'police', 'shopping_mall', 'stadium', 'subway_station', 'supermarket', 'synagogue', 'taxi_stand', 'train_station', 'transit_station', 'zoo'],
+        5:['campground', 'cemetery', 'courthouse', 'lodging', 'rv_park']}
+          
+def find_type(typ):
+    for key in types.keys():
+        if typ in types[key]:
+            return key
 
 def CoordinatesToAdress(coordinates):
     # This function accept coordinates like "30.714546,46.380251" and returs list of possible adresses
     # getting the key from init module
     key = MapInitialize()
-
+    
     link0 ="https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+coordinates+"&type=establishment&language=ru&radius=30&key="+key
     adress=[]
-    output0 = loads(get(link0).text)
-    for ad in range(len(output0['results'])):
-        if output0['results'][ad]['types'][0] in types:
-            name = output0['results'][ad]['name']
-            adr = output0['results'][ad]['vicinity']
-            lat = output0['results'][0]['geometry']['location']['lat']
-            lng = output0['results'][0]['geometry']['location']['lng']
-            typ = output0['results'][ad]['types'][0]
-            adress.append({'name':name, 'adr':adr, 'typ': typ, 'loc':(lat,lng)})
+    output0 = loads(get(link0).text)   
+    if len(output0['results'])!=0:
+        for ad in range(len(output0['results'])):
+            if output0['results'][ad]['types'][0] in (types[1]+types[2]+types[3]+types[4]+types[5]):
+                name = output0['results'][ad]['name']
+                adr = output0['results'][ad]['vicinity']
+                lat = output0['results'][0]['geometry']['location']['lat']
+                lng = output0['results'][0]['geometry']['location']['lng']
+                typ = output0['results'][ad]['types'][0]
+                typ = find_type(typ)
+                adress.append({'name':name, 'adr':adr, 'typ': typ, 'loc':(lat,lng)})
+    else:
+        raise Exception('No company')
 
     link = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+coordinates+"&language=ru&location_type=ROOFTOP&key="+key
     # we open the google page downloading their JSON as str and turning it into python file
     output = loads(get(link).text)
-    for ad in range(len(output['results'])):
-        # itarate and format all available adresses
-        adr = output['results'][ad]['formatted_address']
-        adr = adr.split(" ")
-        adr = adr[:-5]
-        adr = " ".join(adr)
-        adr = adr[:-1]
-        # adress.append({'name': '', 'adr':adr, 'typ': '', 'loc':(lat,lng)})
+    if len(output0['results'])!=0:
+        for ad in range(len(output['results'])):
+            # itarate and format all available adresses
+            adr = output['results'][ad]['formatted_address']
+            adr = adr.split(" ")
+            adr = adr[:-5]
+            adr = " ".join(adr)
+            adr = adr[:-1]
+            lat = output0['results'][0]['geometry']['location']['lat']
+            lng = output0['results'][0]['geometry']['location']['lng']
+            adress.append({'name': adr, 'adr':adr, 'typ': 6, 'loc':(lat,lng)})
+    else:
+        raise Exception('No such adress')
+   
+   
+    # for i in adress:
+    #     print(i)
     return adress
 
 def AdressToCoordinates(adress):
@@ -44,7 +65,7 @@ def AdressToCoordinates(adress):
     key = MapInitialize()
     #adress should be splitted with + and url encoded
     adress = adress.replace(" ","+")
-    adress = urllib.parse.quote_plus(adress)
+    adress = quote_plus(adress)
     link = "https://maps.googleapis.com/maps/api/geocode/json?address="+adress+"&key="+key
     output = loads(get(link).text)
     # getting adress and format it
@@ -62,4 +83,6 @@ def AdressToCoordinates(adress):
 if __name__ == "__main__":
     a = CoordinatesToAdress("46.48402859999999,30.737146")
     for x in a:
-        print(x['name'])
+        print(x['typ'],"\t",x['name'])
+    # CoordinatesToAdress("46.48402859999999,30.737146")
+    # print(AdressToCoordinates('Дерибасовская Ева'))
