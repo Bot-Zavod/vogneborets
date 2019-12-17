b94706057109a68cb38772d35e2de43cc3b90043
Alexey <almarka03@gmail.com>
2019-12-17 21:49:44 +0200

    New class Users for Pooling. More parameters in here

---
+++ ActiveUsers.py
@@ -0,0 +1,59 @@
+class Users():
+	def __init__(self):
+		self.__user_list = []
+
+	def allUsers(self):
+		return self.__user_list
+
+	# add new user
+	def addUser(self, chat_id, name):
+		if self.getUser(chat_id) == False:
+			self.__user_list.append({'chat_id': chat_id, 'name': name, 'status': False, 'PLACES_VARIANT':[], 'USER_PLACE':''})
+		else:
+			return False
+
+	def getUser(self, chat_id):
+		# return user by chat_id
+		for x in self.__user_list:
+			if x['chat_id'] == chat_id:
+				return x
+		# if User is absent
+		return False
+
+	# change user status
+	def changeUserStatus(self, chat_id, status):
+		for x in self.__user_list:
+			if x['chat_id'] == chat_id:
+				x.update({'status': status})
+
+	# change place taht user submit
+	def changeUserPlace(self, chat_id, USER_PLACE):
+			for x in self.__user_list:
+				if x['chat_id'] == chat_id:
+					x.update({'USER_PLACE': USER_PLACE})
+					x.update({'PLACES_VARIANT': []})
+
+	# change variant of place
+	def changePlacesVariant(self, chat_id, PLACES_VARIANT):
+			for x in self.__user_list:
+				if x['chat_id'] == chat_id:
+					x.update({'PLACES_VARIANT': PLACES_VARIANT})
+
+
+
+if __name__ == '__main__':
+
+	users = Users()
+	# add new user
+	users.addUser(1, 'Alexey')
+	users.addUser(2, 'Vlad')
+	users.addUser(3, 'Andrew')
+
+	# get new user
+	print(users.getUser(1))
+
+	# change user parameters
+	users.changeUserStatus(1, True)	
+	users.changeUserPlace(1, 'asdgsfh')
+	users.changePlacesVariant(1, ['asdgsdg','adgasdg','asgasdgsdg'])	
+	print(users.getUser(1))
--- main.py
+++ main.py
@@ -3,31 +3,32 @@ from telegram import InlineKeyboardMarkup, InlineKeyboardButton, KeyboardButton,
 from GeoReverse import CoordinatesToAdress
 from init import BotInitialize
 from time import sleep
+from ActiveUsers import *
 import logging
+
 
 # LOGSLOGSLOGS
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # iMITATE USER_db
-IS_USER_ACTIVE = False
-PLACES_VARIANT = []
-USER_PLACE = ''
 QUEST_1, QUEST_2, QUEST_3, QUEST_4, COMMENT, FINISH = range(6)
 
-
-
+Users = Users()
 
 # start message
 def start_command(update, context):	
 	first_name = update.message.chat.first_name	
+	chat_id = update.message.chat.id
 	sticker = "CAADAgAD_iAAAulVBRgi4A0qOPfBBRYE"
 
 	# Check user mode for correct menu
-	if IS_USER_ACTIVE:
+	if Users.getUser(chat_id) and Users.getUser(chat_id)['status']:
 		text = f"*Добрый день, {first_name}!*\n\nРады вас видеть! Выберите действие из меню ниже."
 		reply_markup = ReplyKeyboardMarkup([['Оценить заведение'],['Проверить'],['Советы при ЧС']], resize_keyboard=True)
 	else:
+		# add new User
+		Users.addUser(chat_id, first_name)
 		text = f"*Добрый день, {first_name}!*\n\nОтправьте нам свое местоположение, тоб мы могли провести оценку места"
 		location_button = KeyboardButton(text="Отправить местоположение", request_location = True)
 		reply_markup = ReplyKeyboardMarkup([[location_button],['Советы при ЧС']], resize_keyboard=True)
@@ -38,60 +39,76 @@ def start_command(update, context):
 
 # Instructions during emergency situations 
 def Instructions(update, context):
+	# add User if he is absent, return False if absent
+	Users.addUser(update.message.chat.id, update.message.chat.first_name)
 	logger.info("User %s: ask instructions", update.message.chat.id)
 	update.message.reply_text(text='*Инструкции при пожаре и других ЧС!*', parse_mode='markdown')
 
+def test(update, context):
+	print(Users.allUsers())
+	update.message.reply_text(text='Читайте логи', parse_mode='markdown')
+
+
 # Location message
 def check_location(update, context):
+	chat_id = update.message.chat.id
 	lon = update.effective_message.location.longitude
 	lat = update.effective_message.location.latitude
 	res = CoordinatesToAdress(str(lat)+','+str(lon))
+	# add User if he is absent, return False if absent
+	Users.addUser(chat_id, update.message.chat.first_name)
+
 	if len(res) > 0:
 		buttons = []
-		global PLACES_VARIANT
+		PLACES_VARIANT = []
 		for x in res:
 			buttons.append([x['name']])
 			PLACES_VARIANT.append(x['name'])
+
 		reply_markup = ReplyKeyboardMarkup(buttons, resize_keyboard=True)
 		update.message.reply_text(text='*В каком именно заведении вы находитесь?*', reply_markup=reply_markup, parse_mode='markdown')
+		# add places to Users
+		Users.changePlacesVariant(chat_id, PLACES_VARIANT)
+
+		# Submit the correct location
+		reg = '^'+'|'.join(PLACES_VARIANT)+'$'
+		logger.info("User %s: send location %s", update.message.chat.id, str(lat)+','+str(lon))
+		dispatcher.add_handler(MessageHandler(Filters.regex(reg), submit_location))
 	else:
 		update.message.reply_text(text='*Что-то я ничего не вижу. Попробуй еще раз!*', parse_mode='markdown')
 
-	# Submit the correct location
-	reg = '^'+'|'.join(PLACES_VARIANT)+'$'
-	logger.info("User %s: send location %s", update.message.chat.id, str(lat)+','+str(lon))
-	dispatcher.add_handler(MessageHandler(Filters.regex(reg), submit_location))
+	
 
 
 # Submit location and turn True mode on User
 def submit_location(update, context):
+	chat_id = update.message.chat.id
 	msg = update.effective_message.text
+
 	# check user mode and correct it
-	global USER_PLACE
-	global IS_USER_ACTIVE
-	if msg in PLACES_VARIANT and IS_USER_ACTIVE==False:		
-		IS_USER_ACTIVE = True
-		USER_PLACE = msg
+	if msg in Users.getUser(chat_id)['PLACES_VARIANT'] and Users.getUser(chat_id)['status']==False:		
+		Users.changeUserStatus(chat_id, True)	
+		Users.changeUserPlace(chat_id, msg)
+
 		text = f'`{update.effective_message.text}`\n\n*Давай теперь оценим его! Для этого тебе надо будет ответить на несколько вопросов. Или ты можешь узнать, что другие думаю про это место*'
 		reply_markup = ReplyKeyboardMarkup([['Оценить заведение'],['Проверить'],['Советы при ЧС']], resize_keyboard=True)
-		logger.info("User %s: submit location '%s' ", update.message.chat.id, USER_PLACE)
+		logger.info("User %s: submit location '%s' ", update.message.chat.id, Users.getUser(chat_id)['USER_PLACE'])
 		update.message.reply_text(text=text, reply_markup=reply_markup, parse_mode='markdown')
 
 # True mode--- place info
 def place_find_output(update, context):
-	if IS_USER_ACTIVE:
-		logger.info("User %s: ask '%s' place info.", update.message.chat.id, USER_PLACE)
-		update.message.reply_text(text=f'Вот все, что у меня есть про это место: {USER_PLACE}', parse_mode='markdown')
+	chat_id = update.message.chat.id
+	if Users.getUser(chat_id) and Users.getUser(chat_id)['status']:
+		logger.info("User %s: ask '%s' place info.", chat_id, Users.getUser(chat_id)['USER_PLACE'])
+		update.message.reply_text(text=f"Вот все, что у меня есть про это место: { Users.getUser(chat_id)['USER_PLACE'] }", parse_mode='markdown')
 
 # True mode--- lets estimate place
 def place_estimation(update, context):
-	if IS_USER_ACTIVE:
-		logger.info("User %s: started estimate '%s' place ", update.message.chat.id, USER_PLACE)
-		update.message.reply_text(text=f'*Давай оценим: {USER_PLACE}*\n\n Ответь на вопросы ниже!', parse_mode='markdown')
+	chat_id = update.message.chat.id
+	if Users.getUser(chat_id) and Users.getUser(chat_id)['status']:
+		logger.info("User %s: started estimate '%s' place ", chat_id, Users.getUser(chat_id)['USER_PLACE'])
+		update.message.reply_text(text=f"*Давай оценим: { Users.getUser(chat_id)['USER_PLACE'] }*\n\n Ответь на вопросы ниже!", parse_mode='markdown')
 		return QUEST_1
-
-
-
 
 
 
@@ -101,17 +118,22 @@ def place_estimation(update, context):
 def q1(update, context):
 	button1 = InlineKeyboardButton('Да ✅', callback_data='yes2')
 	button2 = InlineKeyboardButton('Нет ❌', callback_data='no2')
-	keyboard = InlineKeyboardMarkup([[button1, button2]])
+	button3 = InlineKeyboardButton('Не знаю ❔', callback_data='wtf2')
+	keyboard = InlineKeyboardMarkup([[button1, button2],[button3]])
 	text = "*Вопрос №1*"
 	update.message.reply_text(text=text, parse_mode='markdown', reply_markup=keyboard)
 	return QUEST_2
+
+
+
+
 
 def q2(update, context):
 	query = update.callback_query
 	button1 = InlineKeyboardButton('Да ✅', callback_data='yes3')
 	button2 = InlineKeyboardButton('Нет ❌', callback_data='no3')
-	keyboard = InlineKeyboardMarkup([[button1, button2]])
-
+	button3 = InlineKeyboardButton('Не знаю ❔', callback_data='wtf3')
+	keyboard = InlineKeyboardMarkup([[button1, button2],[button3]])
 	# запись в бд будет
 	if query.data == 'yes2':
 		pass
@@ -126,7 +148,8 @@ def q3(update, context):
 	query = update.callback_query
 	button1 = InlineKeyboardButton('Да ✅', callback_data='yes4')
 	button2 = InlineKeyboardButton('Нет ❌', callback_data='no4')
-	keyboard = InlineKeyboardMarkup([[button1, button2]])
+	button3 = InlineKeyboardButton('Не знаю ❔', callback_data='wtf4')
+	keyboard = InlineKeyboardMarkup([[button1, button2],[button3]])
 
 	if query.data == 'yes3':
 		pass
@@ -141,7 +164,8 @@ def q4(update, context):
 	query = update.callback_query
 	button1 = InlineKeyboardButton('Да ✅', callback_data='yes5')
 	button2 = InlineKeyboardButton('Нет ❌', callback_data='no5')
-	keyboard = InlineKeyboardMarkup([[button1, button2]])
+	button3 = InlineKeyboardButton('Не знаю ❔', callback_data='wtf5')
+	keyboard = InlineKeyboardMarkup([[button1, button2],[button3]])
 
 	if query.data == 'yes4':
 		pass
@@ -179,9 +203,10 @@ def finish_est(update, context):
 
 # End commant of ConversationHandler
 def cancel(update, context):
-	logger.info("User %s: end estimate '%s' by /cansel command ", update.message.chat.id, USER_PLACE)
-    update.message.reply_text(text='Ты можешь попытаться еще раз!',  reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
+	chat_id = update.message.chat.id
+	logger.info("User %s: end estimate '%s' by /cansel command ", chat_id, Users.getUser(chat_id)['USER_PLACE'])
+	update.message.reply_text(text='Ты можешь попытаться еще раз!',  reply_markup=ReplyKeyboardRemove())
+	return ConversationHandler.END
 
 
 if __name__ == "__main__":
@@ -190,6 +215,9 @@ if __name__ == "__main__":
 
 	# Start command handler
 	dispatcher.add_handler(CommandHandler('start', start_command))
+
+	# test command
+	dispatcher.add_handler(CommandHandler('test', test))
 
 	# Instructions and Tips
 	dispatcher.add_handler(MessageHandler(Filters.regex('^Советы при ЧС$'), Instructions))
@@ -213,6 +241,9 @@ if __name__ == "__main__":
 			},
 			fallbacks=[CommandHandler('cancel', cancel)]
 		))
+
+	# Sort all messanges and check User in botUsers
+	# dispatcher.add_handler(MessageHandler(Filters.all, sorter))
 
 
 	# For more comfortable start and stop from console
