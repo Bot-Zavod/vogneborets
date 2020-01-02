5458a4de7d62b4b37d258bedada5d299cbf730bf
Vlad-Vargan <blach.smith.2000@gmail.com>
2020-01-02 22:19:39 +0200

    Modified for deploy

---
+++ init.py
@@ -0,0 +1,14 @@
+
+bot_token = '850462457:AAHRS7lOavs3VgD7_PR-QqcXXSkhnYT8Kho',
+	
+def MapInitialize():
+    key = 'AIzaSyBrQmgLCVlaxO4hGtPlJAzeXE8U7xdufWQ'
+    return key
+
+def connectDB():
+	dbname = 'Twinkly'
+	user = 'twinklyadmin'
+	password = 'tfEJQjqS8wYvH23xEcGH'
+	host = 'twinkly-dev.cc4xdtflaafw.us-west-2.rds.amazonaws.com'
+	port = 5433
+	return (dbname, user, password, host, port)
