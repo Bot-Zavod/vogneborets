cdc4c24696cd6f2e5f8f0b3dfc2117033f226e72
Andrew001207 <andrey001207@gmail.com>
2020-01-22 12:42:19 +0200

    port

--- Website/main.py
+++ Website/main.py
@@ -62,4 +62,4 @@ def about_us():
     return render_template('about_us.html')
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host= '0.0.0.0')
+    app.run(debug=True, use_reloader=True, host= '0.0.0.0', port=8080)
