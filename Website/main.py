72ee09b1b034fb134a7e684c5a504d6ad33269fd
Andrew001207 <andrey001207@gmail.com>
2020-01-22 12:38:46 +0200

    SIte

--- Website/main.py
+++ Website/main.py
@@ -3,15 +3,9 @@
 from flask import Flask, render_template, request, redirect
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
-import stripe
 from TwinklyDb import *
 
 app = Flask(__name__, template_folder="templates")
-
-pub_key = 'pk_test_3LtOdozoRiUg6rH7Jtn5k8tR008gczBxTM'
-secret_key = 'sk_test_6pYkFEXMF33C1y8ldLrS1wi50006quKVd3'
-
-stripe.api_key = secret_key
 
 # you can set key as config
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyBTfQk2e0_GziHCnrhHYcVituFAWEgBIuQ"
@@ -66,29 +60,6 @@ def mapview():
 @app.route("/team", methods=["GET", "POST"])
 def about_us():
     return render_template('about_us.html')
-
-@app.route("/donate")
-def donate():
-    return render_template('donate.html')
-
-@app.route("/donate/<amount>")
-def donate_post_amount(amount):
-    amount = int(amount)
-    return render_template('donate_amount.html', amount = amount)
-
-@app.route('/pay', methods=['POST'])
-def pay():
-    print('Token:' + request.form['stripeToken'])
-    customer = stripe.Customer.create(source=request.form['stripeToken'])
-
-    charge = stripe.Charge.create(
-        customer=customer.id,
-        amount=int(request.form['amount']),
-        currency='uah',
-        description='The Product'
-    )
-
-    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug=True, use_reloader=True, host= '0.0.0.0')
