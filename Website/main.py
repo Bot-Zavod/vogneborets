177bca18aec1f861e5c0319427cbc0016a5acbc8
Andrew001207 <andrey001207@gmail.com>
2020-01-23 13:04:13 +0200

    website

--- Website/main.py
+++ Website/main.py
@@ -3,16 +3,18 @@
 from flask import Flask, render_template, request, redirect
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
+
 from TwinklyDb import *
 
-app = Flask(__name__, template_folder="templates")
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 # you can set key as config
 app.config['GOOGLEMAPS_KEY'] = "AIzaSyBTfQk2e0_GziHCnrhHYcVituFAWEgBIuQ"
 
 # you can also pass key here
 GoogleMaps(
-    app
+    app,
+    #key="AIzaSyDP0GX-Wsui9TSDxtFNj2XuKrh7JBTPCnU"
 )
 
 # NOTE: this example is using a form to get the apikey
@@ -38,28 +40,55 @@ def mapview():
     twmap = Map(
         identifier="twmap",
         varname="twmap",
+        cls='',
         style=(
             "height:100%;"
             "width:100%;"
-            "left:0;"
-            "position:absolute;"
-            # "z-index:200;"            
+            # "top:100%;"
+            # "left:0;"
+            # "position:absolute;"
+            # "z-index:200;"
         ),
         lat=46.44,
         lng=30.75,
         zoom = 15,
-        markers=markers
+        markers=markers,
+        fit_markers_to_bounds=True
     )
 
     return render_template(
-        'main.html',
+        'index.html',
         twmap=twmap,
-        GOOGLEMAPS_KEY=request.args.get('apikey')
     )
 
-@app.route("/team", methods=["GET", "POST"])
-def about_us():
-    return render_template('about_us.html')
+@app.route("/map")
+def map():
+    markers = updateReviews()
+    print(len(markers))
+    twmap = Map(
+        identifier="twmap",
+        varname="twmap",
+        cls='',
+        style=(
+            "height:100%;"
+            "width:100%;"
+            "position:absolute;"
+            "z-index:200;"
+            "top:0;"
+            "left:0;"
+        ),
+        lat=46.44,
+        lng=30.75,
+        zoom = 15,
+        markers=markers,
+        fit_markers_to_bounds=True
+    )
+
+    return render_template(
+        'map.html',
+        twmap=twmap,
+    )
+
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=True, host= '0.0.0.0', port=8080)
+    app.run(debug=True, use_reloader=True, host= '0.0.0.0', port=5001)
